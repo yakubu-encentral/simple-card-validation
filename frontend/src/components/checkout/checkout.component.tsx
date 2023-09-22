5cb2ba1b21aae982f35620be459c0804036b2623
yakubu-encentral <yakubu@encentralsolutions.com>
2023-09-22 16:59:56 +0100

    frontend - created and set checkout components

---
+++ frontend/src/components/checkout/checkout.component.tsx
@@ -0,0 +1,13 @@
+import Footer from "./footer.checkout";
+import Main from "./main.checkout";
+
+const CheckOut = () => {
+  return (
+    <div className="rounded-xl bg-gray-50 shadow-xl">
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default CheckOut;
---
+++ frontend/src/components/checkout/main.checkout.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <>
+      <p>Main</p>
+    </>
+  );
+};
+
+export default Main;
