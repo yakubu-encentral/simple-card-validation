e8ce1aeb9b85d87bc2569b4e6f3b703d3b2ee750
yakubu-encentral <yakubu@encentralsolutions.com>
2023-09-22 16:32:20 +0100

    frontend - configured tailwind and prettier

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
