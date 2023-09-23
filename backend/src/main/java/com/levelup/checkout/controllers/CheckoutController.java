05ec720ffda10efc7b7784e2f6202398e3293268
yakubu-encentral <yakubu@encentralsolutions.com>
2023-09-23 19:04:34 +0100

    backend - added "/api" to url path

--- backend/src/main/java/com/levelup/checkout/controllers/CheckoutController.java
+++ backend/src/main/java/com/levelup/checkout/controllers/CheckoutController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("checkout")
+@RequestMapping("api/checkout")
 public class CheckoutController {
 
     @Autowired
